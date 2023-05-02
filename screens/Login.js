8b2c73fb6b527586c9e34c64b946f43892f99cae
luizatorrens <luiza.torrens.2006@gmail.com>
2023-05-02 15:08:14 -0300

    Criação style login

--- screens/Login.js
+++ screens/Login.js
@@ -1,34 +1,52 @@
 import React from 'react';
-import { TextInput, Button } from 'react-native-paper';
-import {Text, View} from 'react-native'
+import { TextInput, Button, withTheme} from 'react-native-paper';
+import {Text, View, StyleSheet} from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
 
 const Stack = createStackNavigator();
 
-function Login ({ navigation}) {
+function Login ({ navigation, theme}) {
   
     return (
-    <View>
+    <View style= {styles.login}>
         <TextInput
             label="Email"
             mode="outlined"
             right={<TextInput.Affix text="/100" />}
+            style= {styles.emailSenha}
+            roundness= "20"
         />
         <TextInput
             label="Password"
             secureTextEntry
             mode="outlined"
             right={<TextInput.Icon icon="eye" />}
+            style= {styles.emailSenha}
         />
-        <Button icon="camera" mode="contained-tonal" onPress={() => navigation.navigate('Main')}>
+        <Button style= {styles.botaoLogin} icon="arrow-right" mode="contained-tonal" onPress={() => navigation.navigate('Main')}>
     Press me
   </Button>
     </View>
     );
   };
-  
-  export default Login;
+
+  const styles = StyleSheet.create({
+
+    login: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        padding: 20
+    },
+
+    botaoLogin: {
+        marginTop: 20
+    }
+  });
+
+
+  export default Login; withTheme;
 
   
