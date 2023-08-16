7b10093e1c17a6587664bedb7bce82d4ce9e6451
luizatorrens <luiza.torrens.2006@gmail.com>
2023-08-16 11:22:07 -0300

    Selecionando itens do cardápio

--- src/screens/Cardápio.js
+++ src/screens/Cardápio.js
@@ -1,21 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import produtosapi from '../api/produtos';
+import { Checkbox } from 'react-native-paper';
 import axios from 'axios';
 
 function Cardapio() {
   const [produtos, setData] = useState([]);
+  const [checked, setChecked] = React.useState([]);
+
+  function changeCheck(i) {
+    let novoChecked = [...checked]
+    novoChecked[i] = !novoChecked[i]
+    setChecked(novoChecked);
+  }
 
   useEffect(() => {
-    axios.get('http://191.52.55.56:19003/produtos/')
-    .then(response => setData(response.data));
+    
+    async function fetchData()  {
+      const {data}  = await axios.get('http://191.52.55.56:19003/produtos/');
+      setData(data);
+      let novoChecked = []
+      data.map(() => novoChecked.push(false))
+      setChecked(novoChecked)
+    }
+    
+    fetchData();
   }, []);
 
   return (
     <View>
       <Text>Cardápio</Text>
-      {produtos.map(produtos => (
-        <Text>{produtos.titulo}</Text>
+      {produtos.map((produtos, i) => (
+      <View>
+        <Checkbox.Item 
+          label={produtos.titulo}
+          status={checked[i] ? 'checked' : 'unchecked'} 
+          onPress={() => {
+            changeCheck(i)            
+            }} />
+      </View>
       ))}
     </View>
   );
